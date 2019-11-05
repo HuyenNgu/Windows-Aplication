ab85684f6996ee959c99279c3847a1d659ea6c14
HuyenNgu <yurikawsaoki@gmail.com>
2019-11-05 15:34:04 +0700

    load

---
+++ Project1/Project1/MyWindow.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Project1
+{
+    class MyWindow : Form
+    {
+        static void Main()
+        {
+            MyWindow form = new MyWindow();
+            form.Text = "My First Form";
+
+            Button button = new Button();
+            button.Text = "Click me";
+            form.Controls.Add(button);
+
+            Application.Run(form);
+            
+        }
+    }
+}
